428579408bd85fd4568b0c73bba75f803880d198
Ella Mathewson <ellaofohio@gmail.com>
2018-11-01 18:06:49 -0400

    Moved javascript function to home page

--- portfolio/javascript.js
+++ portfolio/javascript.js
@@ -10,26 +10,6 @@
 https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
 */
 var myIndex = 0;
-slideshow();
-
-function slideshow() {
-    //grabs the slideshow 
-    var images = document.getElementsByClassName("slideShow");
-    //loop to remove the images from the HTML
-    for (let i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-    //increases variable
-    myIndex++;
-    //if variable is greater than number of images, resets slideshow to beginning
-    if (myIndex > images.length) {
-        myIndex = 1
-    }
-    //sets the display of the current image to block, shows image on screen
-    images[myIndex - 1].style.display = 'block';
-    //Changes image every 3 seconds
-    setTimeout(slideshow, 3000);
-}
 
 function dropdown() {
     document.getElementById("projDropdown").classList.toggle("show");
